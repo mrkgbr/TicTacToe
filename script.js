9fcb943440ed2205ad3a95c725864ad969d0bea9
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-04-08 17:35:00 +0200

    Add functions and style, refactoring code

--- script.js
+++ script.js
@@ -1,45 +1,82 @@
-const gameBoard = {
-  board: ["", "", "", "", "", "", "", "", ""],
-  cells: document.querySelectorAll(".cell"),
-  render() {
-    this.cells.forEach((cell, index) => {
+const gameBoard = (() => {
+  const board = ["", "", "", "", "", "", "", "", ""];
+  const cells = document.querySelectorAll(".cell");
+  let currentMark = "X";
+  const div = document.querySelector(".header");
+
+  const toggleMark = () => {
+    if (currentMark === "X") {
+      currentMark = "O";
+    } else {
+      currentMark = "X";
+    }
+  };
+
+  const render = () => {
+    cells.forEach((cell, index) => {
       const newCell = cell;
-      newCell.textContent = gameBoard.board[index];
+      newCell.textContent = board[index];
+      newCell.classList.remove("inactive");
     });
-  },
-};
+  };
 
-// const render = (() => {
-//   const cells = document.querySelectorAll(".cell");
+  const checkForWinner = () => {
+    const sayWinner = (winner) => {
+      div.textContent = `The winner is: ${winner}`;
+      cells.forEach((cell) => {
+        const newCell = cell;
+        newCell.classList.add("inactive");
+      });
+    };
 
-//   const board = () => {
-//     cells.forEach((cell, index) => {
-//       const newCell = cell;
-//       newCell.textContent = gameBoard.board[index];
-//     });
-//   };
-//   board();
-//   return { board };
-// })();
+    for (let i = 0; i < board.length; i += 3) {
+      if (
+        board[i] !== "" &&
+        board[i] === board[i + 1] &&
+        board[i] === board[i + 2]
+      ) {
+        sayWinner(board[i]);
+      }
+    }
+    for (let i = 0; i < 3; i += 1) {
+      if (
+        board[i] !== "" &&
+        board[i] === board[i + 3] &&
+        board[i] === board[i + 6]
+      ) {
+        sayWinner(board[i]);
+      }
+    }
+    if (board[0] !== "" && board[0] === board[4] && board[0] === board[8]) {
+      sayWinner(board[0]);
+    }
+    if (board[2] !== "" && board[2] === board[4] && board[2] === board[6]) {
+      sayWinner(board[2]);
+    }
+  };
 
-let currentMark = "X";
+  cells.forEach((cell, index) => {
+    cell.addEventListener("click", () => {
+      board[index] = currentMark;
+      render();
+      toggleMark();
+      checkForWinner();
+      cell.classList.add("inactive");
+    });
 
-const toggleMark = () => {
-  if (currentMark === "X") {
-    currentMark = "O";
-  } else {
-    currentMark = "X";
-  }
-};
-
-gameBoard.cells.forEach((cell, index) => {
-  cell.addEventListener("click", () => {
-    gameBoard.board[index] = currentMark;
-    gameBoard.render();
-    toggleMark();
-    cell.replaceWith(cell.cloneNode(true));
+    const reset = document.querySelector(".reset");
+    reset.addEventListener("click", () => {
+      board.forEach((item, i) => {
+        currentMark = "X";
+        board[i] = "";
+        div.textContent = "";
+      });
+      render();
+    });
   });
-});
+
+  return { render };
+})();
 
 const Player = (name, mark) => {
   const sayName = () => console.log(`my name is ${name}`);
@@ -47,12 +84,7 @@ const Player = (name, mark) => {
   return { sayName, sayMark };
 };
 
-gameBoard.render();
-
 const playerOne = Player("Player 1", "X");
 const playerTwo = Player("Player 2", "O");
 
-playerOne.sayName();
-playerOne.sayMark();
-playerTwo.sayName();
-playerTwo.sayMark();
+gameBoard.render();
